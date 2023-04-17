2aaefacbdbdef6fb5381479b527250465ba542c2
dhryshyn <dev.dmytroh@gmail.com>
2023-04-17 16:27:46 +0300

    Test commit

--- test/index.tsx
+++ test/index.tsx
@@ -1,6 +1,6 @@
 import { CompatRoute } from 'react-router-dom-v5-compat';
 import React from 'react';
-console.log('tessesdfsdft');
+console.log('tessesdfsdfsdfsdft');
 const test = () => {
 	return <div>test</div>;
 };
